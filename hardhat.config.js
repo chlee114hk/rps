79fd1cb41b4553c86e11aea48eb486dce5a79826
Vincent Lee <chlee114hk@gmail.com>
2023-06-25 09:58:16 +0800

    Move hardcoded chainlink config in deploy script to hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -34,33 +34,59 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
  module.exports = {
-    networks: {  
+    networks: {
       hardhat: {
-        initialBaseFeePerGas: 0
+        initialBaseFeePerGas: 0,
+        chainlink: {
+          keyHash: '0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc',
+          minimumRequestConfirmations: 3,
+        },
       },  
       localhost: {
+        url: 'http://127.0.0.1:8545',
         chainlink: {
           keyHash: '0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc',
           minimumRequestConfirmations: 3,
         },
       },
-      rinkeby: {
-        url: `https://rinkeby.infura.io/v3/${infura_id}`,
+      mainnet: {
+        url: `https://mainnet.infura.io/v3/${infura_id}`,
         accounts:
           // process.env.PRIVATE_KEY !== undefined ? [`0x${privateKey}`] : [],
           process.env.KEY_MNEMONIC !== undefined ? { mnemonic: mnemonic} : {},        
+        chainlink: {
+          subscriptionId: 2963,
+          vrfCoordinatorContract: "	0x271682DEB8C4E0901D1a1550aD2e64D568E69909",
+          keyHash: "	0x8af398995b04c28e9951adb9721ef74c74f93e6a478f39e7e0777be13527e7ef",
+          linkTokenContract: '0x514910771AF9Ca656af840dff83E8264EcF986CA',
+          minimumRequestConfirmations: 3,
+        }
       },
       goerli: {
         url: `https://goerli.infura.io/v3/${infura_id}`,
         accounts:
           // process.env.PRIVATE_KEY !== undefined ? [`0x${privateKey}`] : [],
-          process.env.KEY_MNEMONIC !== undefined ? { mnemonic: mnemonic} : {},        
+          process.env.KEY_MNEMONIC !== undefined ? { mnemonic: mnemonic} : {},
+        chainlink: {
+          subscriptionId: 2963,
+          vrfCoordinatorContract: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
+          keyHash: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
+          linkTokenContract: "0x326C977E6efc84E512bB9C30f76E30c160eD06FB",
+          minimumRequestConfirmations: 3,
+        }        
       },
       sepolia: {
         url: `https://sepolia.infura.io/v3/${infura_id}`,
         accounts:
           // process.env.PRIVATE_KEY !== undefined ? [`0x${privateKey}`] : [],
-          process.env.KEY_MNEMONIC !== undefined ? { mnemonic: mnemonic} : {}, 
+          process.env.KEY_MNEMONIC !== undefined ? { mnemonic: mnemonic} : {},
+        chainlink: {
+          subscriptionId: 2963,
+          vrfCoordinatorContract: "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625",
+          keyHash: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c",
+          linkTokenContract: "0x779877A7B0D9E8603169DdbD7836e478b4624789",
+          minimumRequestConfirmations: 3,
+        }
       }
     },
     solidity: {
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,7 @@
-const { hre, ethers } = require("hardhat");
+const hre = require("hardhat");
 const { Signer } = require("ethers");
 const { hexValue } = require("ethers/lib/utils");
+const { networks: networkConfig } = require('../hardhat.config.js');
 
 const main = async () => {
     // Hardhat always runs the compile task when running scripts with its command
@@ -10,6 +11,27 @@ const main = async () => {
     // manually to make sure everything is compiled
     // await hre.run('compile');
 
+    const networkName =  hre.network.name;
+    const config = networkConfig[networkName];
+    let chainlink = config.chainlink;
+
+    chainlink = {
+        keyHash: ethers.constants.HashZero,
+        vrfCoordinatorContract: ethers.constants.AddressZero,
+        ...chainlink
+    }
+
+    console.log("chainlink config:", chainlink)
+    /**
+     * Validate Chainlink Fields
+     */
+    if (!chainlink) throw new Error('Missing chainlink');
+    if (chainlink.subscriptionId === 0)
+        throw new Error('Missing chainlink subscriptionId');
+    if (!chainlink.keyHash) throw new Error('Missing chainlink keyHash');
+    if (!chainlink.vrfCoordinatorContract)
+        throw new Error('Missing chainlink vrfCoordinatorContract');
+
     const [deployer] = await ethers.getSigners();
     console.log(`Address deploying the contract --> ${deployer.address}`);
     console.log("Account balance:", ethers.utils.formatEther(await deployer.getBalance()));
@@ -18,12 +40,21 @@ const main = async () => {
 
     const rps1 = await ethers.getContractFactory("Rps1");
     const rps1Contract = await rps1.deploy(
-        2963,
-        "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625",
-        "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c"
+        chainlink.subscriptionId,
+        chainlink.vrfCoordinatorContract,
+        chainlink.keyHash
     );
     await rps1Contract.deployed();
     console.log(`Rps1 Contract address --> ${rps1Contract.address}`);
+
+    const rps2 = await ethers.getContractFactory("Rps2");
+    const rps2Contract = await rps2.deploy(
+        chainlink.subscriptionId,
+        chainlink.vrfCoordinatorContract,
+        chainlink.keyHash
+    );
+    await rps2Contract.deployed();
+    console.log(`Rps2 Contract address --> ${rps2Contract.address}`);
 }
 
 main()
